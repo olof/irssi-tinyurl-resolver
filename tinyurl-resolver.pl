0ffeba0dc6560f83564121d89f479253fc56e809
Olof Johansson <olof@ethup.se>
2012-01-16 21:12:54 +0100

    Prepare for release 0.61

--- tinyurl-resolver.pl
+++ tinyurl-resolver.pl
@@ -20,7 +20,7 @@ use LWP::UserAgent;
 use Regexp::Common qw/URI/;
 use Irssi;
 
-my $VERSION = '0.6';
+my $VERSION = '0.61';
 my %IRSSI = (
 	authors     => "Olof 'zibri' Johansson",
 	contact     => 'olof@ethup.se',
